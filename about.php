7e75c4494026c4cc078a9a6aa4a3b2b2f8f21107
Digambar Babali Kothawale <digambarkothawale05@gmail.com>
2023-03-07 14:11:52 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <p></p>
+                            <ul class="row">
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                  
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                   
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                   
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
---
+++ footer.php
@@ -0,0 +1,62 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>This is the demo project created for Complete Hospital Management System Where three Important modules of hospital such as Admin,Doctors and Patients can consume the desired services with each other in fluent manner.We are not professional developers but we tried our best to developed this system.We are open to suggestion.If there any. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p>Yashwantrao Bhonsale Polytechnic</p>
+            <ul>
+              <li class="mobile">
+                <p>1010 2020 36360</p>
+                <p>9-269-690-HMS</p>
+              </li>
+              <li class="email">
+                <p>digambar@gmail.com</p>
+                <p>info@hmsphp.com</p>              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Hospital Management System - Application Developed By <span style="color: white"> Our Team</span></p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
